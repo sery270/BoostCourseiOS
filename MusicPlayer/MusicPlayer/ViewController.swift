8a226c3dffb8d0436d9d654eff4c4554746da516
sery270 <sery270@naver.com>
2020-08-18 12:59:16 +0900

    [Chore] gitignore 추가

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 박세란 on 2020/08/18.
+//  Copyright © 2020 박세란. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
